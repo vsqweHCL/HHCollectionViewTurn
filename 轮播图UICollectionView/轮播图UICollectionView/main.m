e481328d9c099770872bcd4eccb22971160d51fa
HCL黄 <hclhuang@HCLhuangdeMacBook-Pro.local>
2016-11-06 12:54:04 +0800

    初始化项目

---
+++ 轮播图UICollectionView/轮播图UICollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  轮播图UICollectionView
+//
+//  Created by HCL黄 on 16/11/6.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 轮播图UICollectionView/轮播图UICollectionViewTests/___UICollectionViewTests.m
@@ -0,0 +1,39 @@
+//
+//  ___UICollectionViewTests.m
+//  轮播图UICollectionViewTests
+//
+//  Created by HCL黄 on 16/11/6.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___UICollectionViewTests : XCTestCase
+
+@end
+
+@implementation ___UICollectionViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
